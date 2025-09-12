8421d4a8add73626ef3b62a8fe5843d87d23ae41
Akash <akashbbu@gmail.com>
2025-09-12 17:14:01 +0530

    home page layout setup

--- src/app/(dashboard)/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-
-export default function Home() {
-  return (
-<div className="flex items-center justify-center"><Button>Go To Dashboard</Button></div>
-  );
-}
