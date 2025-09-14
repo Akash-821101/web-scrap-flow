6793c56d34805ec49f095c5bc6e874b8a52f7f36
Akash <akashkumar.singh09dev@gmail.com>
2025-09-14 10:56:23 +0530

    clerk auth setup completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AppProviders from "@/components/providers/AppProviders";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider 
+    appearance={{
+      elements: {
+        formButtonPrimary: "bg-primary hover:bg-primary/90 text-sm !shadow-none",
+      }
+    }}
+    afterSignOutUrl={"/sign-in"}>
     <html lang="en">
-      <body className={inter.className}><AppProviders>{children}</AppProviders></body>
+      <body className={inter.className}>
+        <AppProviders>{children}</AppProviders>
+        </body>
     </html>
+    </ClerkProvider>
   );
 }
